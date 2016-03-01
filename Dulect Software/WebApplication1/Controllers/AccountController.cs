155e6eee154850c566ebace4a9ce7fad0c524d63
kamrul043 <kamrul.hasan043@gmail.com>
2016-03-01 23:57:28 +0600

    Just create and add project
    Dulcet software work on a company website

---
+++ Dulect Software/WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Web.Security;
+
+namespace WebApplication1.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(tbl_User U)
+        {
+            AuthenticationEntities DE = new AuthenticationEntities();
+            var Count = DE.tbl_User.Where(x => x.UserName == U.UserName && x.Password == U.Password).Count();
+            if (Count == 0)
+            {
+                ViewBag.Msg = "Invalid User";
+                return View();
+
+            }else
+            {
+                FormsAuthentication.SetAuthCookie(U.UserName, false);
+                return RedirectToAction("Index","Home");
+            }
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ Dulect Software/WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private AuthenticationEntities db = new AuthenticationEntities();
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var tbl_Emp = db.tbl_Emp.Include(t => t.tbl_Dept);
+            return View(tbl_Emp.ToList());
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Emp tbl_Emp = db.tbl_Emp.Find(id);
+            if (tbl_Emp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Emp);
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            ViewBag.DeptId = new SelectList(db.tbl_Dept, "DId", "DName");
+            return View();
+        }
+
+        // POST: Employee/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EId,EName,Salary,Dob,Gender,DeptId")] tbl_Emp tbl_Emp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbl_Emp.Add(tbl_Emp);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DeptId = new SelectList(db.tbl_Dept, "DId", "DName", tbl_Emp.DeptId);
+            return View(tbl_Emp);
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Emp tbl_Emp = db.tbl_Emp.Find(id);
+            if (tbl_Emp == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DeptId = new SelectList(db.tbl_Dept, "DId", "DName", tbl_Emp.DeptId);
+            return View(tbl_Emp);
+        }
+
+        // POST: Employee/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EId,EName,Salary,Dob,Gender,DeptId")] tbl_Emp tbl_Emp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_Emp).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DeptId = new SelectList(db.tbl_Dept, "DId", "DName", tbl_Emp.DeptId);
+            return View(tbl_Emp);
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Emp tbl_Emp = db.tbl_Emp.Find(id);
+            if (tbl_Emp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Emp);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbl_Emp tbl_Emp = db.tbl_Emp.Find(id);
+            db.tbl_Emp.Remove(tbl_Emp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
